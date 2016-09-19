8e66f3815ec5e355396ace8d81b2fdff92030074
Chris Mayer <mayerxc@gmail.com>
2016-09-18 20:56:20 -0400

    added changes to switch between break and work clock

--- js/index.js
+++ js/index.js
@@ -1,14 +1,17 @@
 var myTimer
 var min, sec;
-var minStr, secStr
+var initMin, initSec, initBreakMin, initBreakSec;
 var breakMin, breakSec;
 var breakMinStr, breakSecStr
 var isPaused = true;
+var pomo = "work";
+var alarm = new Audio('http://res.cloudinary.com/mayerxc/video/upload/v1473729116/Cuckoo-clock-sound_of6pmy.mp3');
 min = 25;
 sec = 0;
 breakMin = 5;
 breakSec = 0;
-
+initMin = 25;
+initBreakMin= 5;
 
 
 
@@ -19,28 +22,48 @@ function loadPage() {
 }
 
 function timer() {
+
     myTimer = setInterval(runTimer, 1000)
 }
 
 function runTimer() {
     if (!isPaused) {
-        if (sec === 0) {
-            min -= 1;
-            sec = 60;
+        if (pomo === "work") {
+            if (sec === 0) {
+                if (min === 0) {
+                    alarm.play();
+                    pomo = "break"
+                }
+                min -= 1;
+                sec = 60;
+            }
+            sec -= 1;
+            $(".pomodoro p").html(("0" + min).slice(-2) + ":" + ("0" + sec).slice(-2));
+            if (pomo==="break"){
+                min= initMin
+            }
         }
-
-        sec -= 1;
-        console.log(sec);
-        if (min == 0) {
-            //set off alarm
+        if (pomo === "break") {
+            if (breakSec === 0) {
+                if (breakMin === 0) {
+                    alarm.play();
+                    pomo = "work"
+                }
+                breakMin -= 1;
+                breakSec = 60;
+            }
+            breakSec -= 1;
+            $(".pomodoro p").html(("0" + breakMin).slice(-2) + ":" + ("0" + breakSec).slice(-2));
+            if (pomo === "work") {
+                breakMin = initBreakMin;
+            }
         }
     }
-
-    $(".pomodoro p").html(("0" + min).slice(-2) + ":" + ("0" + sec).slice(-2));
 }
 
 function addToTimer() {
     min += 1;
+    initMin = min;
     $(".pomodoro p").html(("0" + min).slice(-2) + ":" + ("0" + sec).slice(-2));
     $(".timer").html(("0" + min).slice(-2) + ":" + ("0" + sec).slice(-2));
 
@@ -51,21 +74,24 @@ function minusTimer() {
     if (min <= 0) {
         min = 1;
     }
+    initMin = min;
     $(".pomodoro p").html(("0" + min).slice(-2) + ":" + ("0" + sec).slice(-2));
     $(".timer").html(("0" + min).slice(-2) + ":" + ("0" + sec).slice(-2));
 }
 
 function addToBreakTimer() {
     breakMin += 1;
-    //$(".pomodoro p").html(("0" + min).slice(-2) + ":" + ("0" + sec).slice(-2));
+    initBreakMin = breakMin
+        //$(".pomodoro p").html(("0" + min).slice(-2) + ":" + ("0" + sec).slice(-2));
     $(".break").html(("0" + breakMin).slice(-2) + ":" + ("0" + breakSec).slice(-2));
 }
 
-function minusBreakTimer (){
+function minusBreakTimer() {
     breakMin -= 1;
     if (breakMin <= 0) {
         breakMin = 1;
     }
+    initBreakMin = breakMin
     $(".break").html(("0" + breakMin).slice(-2) + ":" + ("0" + breakSec).slice(-2));
 }
 
@@ -76,12 +102,12 @@ $(document).ready(function () {
     loadPage();
 
     $(".pomodoro").click(function () {
-        
+
         if (isPaused) {
             isPaused = false;
         } else {
             isPaused = true;
-            
+
         }
         //clearInterval(myTimer) so it doesn't count down too fast everytime I click the div
         clearInterval(myTimer);
@@ -97,12 +123,12 @@ $(document).ready(function () {
         e.preventDefault;
         minusTimer();
     });
-    
+
     $(".break-plus").click(function (e) {
         e.preventDefault;
         addToBreakTimer();
     });
-    
+
     $(".break-minus").click(function (e) {
         e.preventDefault;
         minusBreakTimer();
